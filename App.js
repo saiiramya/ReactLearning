0ba22d38c65cf7d2ce367384b8afcf731d092f09
Ramya <sairamya.nallamilli@cyient.com>
2023-08-16 16:18:43 +0530

    initia commit

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "avc" },
+//   "Hello from React"
+// );
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+  ])
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
